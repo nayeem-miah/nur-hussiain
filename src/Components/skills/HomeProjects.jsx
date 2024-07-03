fb7360437a4a14959dcd80c3f39857928935a9d4
nayeem-miah <nayeem5113a@gmail.com>
2024-07-04 04:29:13 +0600

    start-contact-page

--- src/Components/skills/HomeProjects.jsx
+++ src/Components/skills/HomeProjects.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ProjectCard from "../../Pages/Projects/ProjectCaed";
+import { Link } from "react-router-dom";
 
 const HomeProjects = () => {
   const [project, setProject] = useState([]);
@@ -14,13 +15,21 @@ const HomeProjects = () => {
   const pri = project.slice(0, 2);
   //   console.log(pri.length);
   return (
-    <div>
+    <div className="bg-[#dfdfda] py-11">
       <h3 className="text-3xl text-center font-bold"> My Projects </h3>
-      <div className="grid lg:grid-cols-2 grid-cols-1">
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-7 my-7">
         {pri?.map(pro => (
           <ProjectCard key={pro.name} project={pro}></ProjectCard>
         ))}
       </div>
+      <div class="flex justify-center items-center py-5">
+          <Link
+            to={"/projects"}
+            className="bg-gradient-to-r from-blue-500 via-green-500 to-purple-500 hover:bg-gradient-to-r hover:from-purple-500 hover:via-pink-500 hover:to-red-500 text-white font-bold py-2 px-4 rounded"
+          >
+            Show All Projects
+          </Link>
+          </div>
     </div>
   );
 };
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,11 +1,69 @@
-import React from 'react';
-
+import { MdOutlineEmail } from "react-icons/md";
+import { IoCallSharp } from "react-icons/io5";
+import { FaFacebookF, FaGithub, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { BiSolidDownArrow } from "react-icons/bi";
 const Contact = () => {
-    return (
-        <div>
-            Contact
+  return (
+    <div>
+      <h3 className="text-3xl font-bold text-center my-5">Contact ME </h3>
+      <p>
+        I'm always open to discussing new projects, creative ideas, or
+        opportunities to be part of your vision. Feel free to reach out through
+        any of the following methods:
+      </p>
+      <div>
+        <p className="lg:text-2xl text-xl font-serif my-5">
+          <span className="font-bold">name:</span> MD Nayeem Miah
+        </p>
+        <div className="font-bold text-xl">
+          <p className="flex items-center gap-3  lg:text-xl text-xs">
+            <MdOutlineEmail></MdOutlineEmail>email:{" "}
+            <a
+              href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRnZnPXBQlhpDrpZbSqrsmxJlXSQvsKJKlvnPDWNSHcqJtPMwhhZlPnNCsWVPFhPhLNdnLv"
+              target="_blank"
+              className="hover:link-hover hover:text-blue-500 "
+            >
+              nayeem5113a@gamil.com
+            </a>
+          </p>
+          <p className="flex items-center my-2 lg:text-xl text-xs">
+            <IoCallSharp></IoCallSharp> <span>contact Number:</span>{" "}
+            +8801849317388
+          </p>
         </div>
-    );
+        <div>
+          <h4 className=" flex items-center lg:text-xl text-xs">
+            Let's connect on social networks
+            <BiSolidDownArrow></BiSolidDownArrow>
+          </h4>
+          <nav>
+            <div className="grid grid-cols-3 ">
+              <Link
+                to={"https://github.com/nayeem-miah/nayeem-miah"}
+                target="_blank"
+              >
+                <FaGithub className="text-4xl"></FaGithub>
+              </Link>
+
+              <a
+                href="https://linkedin.com/in/https://www.linkedin.com/in/nayeem-islam-734719307"
+                target="blank"
+              >
+                <FaLinkedin className="text-4xl text-blue-600"></FaLinkedin>
+              </a>
+              <a
+                href="https://fb.com/https://web.facebook.com/profile.php?id=100042968569904&_rdc=1&_rdr"
+                target="blank"
+              >
+                <FaFacebookF className="text-4xl text-blue-600"></FaFacebookF>
+              </a>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
